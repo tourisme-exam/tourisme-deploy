3eed8b66b6cb300b1a63fd7d7d3335aeda48665c
NainaMatthieu <garagenotificiation@gmail.com>
2023-08-05 01:17:59 +0300

    vercel.json

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 require('./util/DBConnexion');
 
 const app = express();
-const certFilePath = './certificat/certificate.pem';
-const keyFilePath = './certificat/privatekey.pem';
-const passphrase = 'to1rism2ex0mf1nAL';
-
-// Options serveur HTTPS
-const options = {
-  cert: fs.readFileSync(certFilePath),
-  key: fs.readFileSync(keyFilePath),
-  passphrase: passphrase
-};
-
-// const server = https.createServer(options, app);
+app.use(express.static('public'));
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -29,7 +16,8 @@ const CategorieRoute = require('./router/CategorieRoute');
 app.use('/categorie', CategorieRoute);
 
 const UserRoute = require('./router/UtilisateurRoute');
-// const InsertionData = require('./util/insertionData');
-// InsertionData();
 app.use('/user', UserRoute);
-app.listen(9000);
+
+app.listen(3000, () => {
+  console.log('Le serveur est en écoute sur le port 3000.');
+});
