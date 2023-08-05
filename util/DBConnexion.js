20f2360a1689261a49cafae6cc3e3de8a041b125
NainaMatthieu <garagenotificiation@gmail.com>
2023-08-05 08:41:42 +0300

    delete img

--- util/DBConnexion.js
+++ util/DBConnexion.js
@@ -4,7 +4,7 @@ require('dotenv/config');
 // mongoose.set('debug', true);
 async function connectToDatabase() {
   try {
-    await mongoose.connect(process.env.DB_URL, {
+    await mongoose.connect("mongodb+srv://tourisme:tourisme1234@tourisme.xynbuya.mongodb.net/tourisme", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
